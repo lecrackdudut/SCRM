8df1bcb30ba2973599949b4b45e14f273445723b
Thomas Tse <thomasxd24@hotmail.com>
2022-05-25 15:38:33 +0200

    added project

---
+++ resources/js/Pages/Projects.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Button from 'primevue/button';
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Projets
+            </h2>
+        </template>
+
+        <div class="py-6">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end">
+                    <div>
+                       <Button label="Creer un projet" />
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::get('/projects', function () {
+        return Inertia::render('Projects');
+    })->name('projects');
 });
